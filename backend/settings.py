26e5aadd365ccd01e2abc17cf69f0ab512b31d4b
rajagokilab <rajagokilab@gmail.com>
2025-09-13 17:07:32 +0200

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:8000",
     "http://localhost:5173",
-    "https://bikefrontend-nu.vercel.app" 
+    "https://bikefrontend-nu.vercel.app" ,
      "https://bikebackend-4.onrender.com",# React dev server
 ]
 
